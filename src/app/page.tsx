fbad08db9ec811d60ef0ba47a818447afab6b32d
nabeel <nabeelknabeel123@gmail.com>
2025-05-15 12:20:06 +0530

    update-practice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,7 @@ import React from 'react'
 function page() {
   return (
     <div>
-     <video src="shanik.mp4" autoPlay controls muted loop width="300">
-  Your browser does not support the video tag.
-</video>
-
+<h1>practice</h1>
     </div>
   )
 }
